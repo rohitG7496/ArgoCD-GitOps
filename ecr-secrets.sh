de7f34ab7bdeaf1e21fedc145a5b90bc6adbf45c
rohit <rogaikwad96@gmail.com>
2025-11-23 15:47:29 +0530

    added image pull secrets in deployment config

--- ecr-secrets.sh
+++ ecr-secrets.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Ensure the namespace exists
+kubectl create namespace db --dry-run=client -o yaml | kubectl apply -f -
+
+# Create or update the secret
 kubectl create secret docker-registry ecr-secret \
     --docker-server=120496071282.dkr.ecr.ap-south-1.amazonaws.com \
     --docker-username=AWS \
     --docker-password="$(aws ecr get-login-password --region ap-south-1)" \
-    --namespace db
+    --namespace db \
+    --dry-run=client -o yaml | kubectl apply -f -
